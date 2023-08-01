f0c3fa252cf3546206c652e5d2eef19caa381822
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-08-01 20:45:32 -0300

    JS 01 - Intro

---
+++ js-01/js/script.js
@@ -0,0 +1,15 @@
+var num = 1;
+console.log("Hola JS");
+// debugger;
+num = 3;
+
+// alert("Hola JS");
+
+// const result = confirm("¿Esta seguro?");
+// console.log(result);
+
+// console.table({ nombre: "Juan", edad: 33 });
+console.log("Hola);
+
+// const dato = prompt("Ingrese un dato");
+// console.error("El dato es: " + dato);
